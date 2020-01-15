ea606ca56d450c6b04d1fe38cc3b022c216f0287
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-15 13:57:10 +0800

    更新代码

--- src/main/java/study/springboot/security/rest/controller/DemoController.java
+++ src/main/java/study/springboot/security/rest/controller/DemoController.java
@@ -1,13 +1,17 @@
 package study.springboot.security.rest.controller;
 
+import com.google.common.collect.Maps;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.springboot.security.rest.support.Results;
+
+import java.util.Map;
 
 @RestController
 public class DemoController {
 
     @GetMapping("/demo")
-    public String demo() {
-        return "demo";
+    public Map<String, Object> demo() {
+        return Results.ok();
     }
 }
