34f80cfb891cd4a2067009c934b583155a69c892
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-15 13:46:13 +0800

    更新代码

--- src/main/java/study/springboot/security/rest/auth/details/UserDetailsServiceImpl.java
+++ src/main/java/study/springboot/security/rest/auth/details/UserDetailsServiceImpl.java
@@ -13,7 +13,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("======> loadUserByUsername");
-        
         CustomUserDetails userDetails = new CustomUserDetails("wzj", "123");
         return userDetails;
     }
