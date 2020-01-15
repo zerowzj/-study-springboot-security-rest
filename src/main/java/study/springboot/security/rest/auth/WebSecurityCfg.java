e92237e26a5d9e0f90c19ef6c03c79026fb0f174
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-15 10:34:22 +0800

    更新代码

--- src/main/java/study/springboot/security/rest/auth/WebSecurityCfg.java
+++ src/main/java/study/springboot/security/rest/auth/WebSecurityCfg.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,6 +30,12 @@ public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
     private UserDetailsService userDetailsService;
     @Autowired
     private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.debug(true);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
