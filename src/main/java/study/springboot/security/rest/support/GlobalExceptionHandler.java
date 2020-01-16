4163bcb7c1dfe5877f656d57cdc9f85c2591de0c
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-16 13:25:36 +0800

    更新代码

---
+++ src/main/java/study/springboot/security/rest/support/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package study.springboot.security.rest.support;
+
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Throwable.class)
+    public Map<String, Object> resolveException(Exception ex) {
+        log.error("发生异常====>", ex);
+        Map<String, Object> data = Maps.newHashMap();
+        data.put("code", "9999");
+        data.put("desc", ex.getMessage());
+        return data;
+    }
+}
